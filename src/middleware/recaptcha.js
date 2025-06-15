78c14b1533e34731a432103eb878f7f48741688f
Yohanes Januarico Andiawan <yohan.loadout@gmail.com>
2025-06-15 17:49:22 +0700

    added ssl

--- src/middleware/recaptcha.js
+++ src/middleware/recaptcha.js
@@ -1,6 +1,11 @@
 const axios = require("axios");
 
 const verifyRecaptcha = async (req, res, next) => {
+  const isProduction = process.env.NODE_ENV === "production";
+  if (!isProduction) {
+    next();
+  }
+
   const recaptchaToken = req.body["recaptchaResponse"];
   if (!recaptchaToken) {
     return res.status(400).json({ message: "Recaptcha token is missing" });
