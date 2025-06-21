877e723e09ce11f7e402ba850b7ed0389cbec0a5
Yohanes Januarico Andiawan <yohan.loadout@gmail.com>
2025-06-21 22:30:19 +0700

    update db_connection

--- src/config/db_connection.js
+++ src/config/db_connection.js
@@ -7,7 +7,9 @@ const dbConnection = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  // add ssl here during prod
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 module.exports = dbConnection.promise();
